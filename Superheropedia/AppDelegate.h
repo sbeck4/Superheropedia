a488b8ce69c78ace1f596325058cc7140b2b817d
Shannon Beck <Shannon.e.beck@gmail.com>
2015-01-19 10:42:56 -0800

    Initial Commit

---
+++ Superheropedia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Superheropedia
+//
+//  Created by Shannon Beck on 1/19/15.
+//  Copyright (c) 2015 Shannon Beck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Superheropedia/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Superheropedia
+//
+//  Created by Shannon Beck on 1/19/15.
+//  Copyright (c) 2015 Shannon Beck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
